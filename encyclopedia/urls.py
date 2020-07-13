2870bf9c7cec7a9c8723533fa813d605808e2b98
sagar-13 <sagar_13@live.com>
2020-07-13 18:12:51 -0400

    mostly done

---
+++ encyclopedia/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("wiki/<str:wiki_name>", views.wiki, name="wiki"),
+    path("search", views.search, name="search"),
+    path("new", views.new, name="new"),
+    path("wiki/<str:wiki_name>/edit", views.edit, name="edit"),
+    
+
+]
---
+++ encyclopedia/views.py
@@ -0,0 +1,133 @@
+from django.shortcuts import render
+
+from . import util
+import markdown2
+from django import forms
+from django.core.files.storage import default_storage
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.core.files import File
+from django.contrib import messages
+
+class NewWikiForm(forms.Form):
+    title = forms.CharField(label="Title", required=True)
+    markdown = forms.CharField(widget=forms.Textarea, required=True)
+    
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def random(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def wiki(request, wiki_name):
+    entry = util.get_entry(wiki_name)
+
+    if entry: 
+
+        return render(request, "encyclopedia/wiki.html", {
+            "entry": markdown2.markdown(entry),
+            "entry_name": wiki_name
+
+        })
+    else:
+        return render(request, "encyclopedia/error.html")
+
+def search(request):
+    query = request.GET.get('q','')
+    entry = util.get_entry(query)
+
+    if entry: 
+
+        return render(request, "encyclopedia/wiki.html", {
+            "entry": markdown2.markdown(entry)
+        })
+    else:
+        results = []
+        for entry in util.list_entries():
+            if query.lower() in entry.lower():
+                results.append(entry)
+
+        # if not results:
+        #     results = ["Sorry, the entry you requested does not exist."]
+        
+        return render(request, "encyclopedia/search.html" , {
+            "results": results
+        })
+
+def new(request):
+
+
+    if request.method == "POST":
+        form = NewWikiForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            markdown = form.cleaned_data["markdown"]
+            
+            try: 
+                with open("./entries/" + title +".md", 'x') as f:
+                    myfile = File(f)
+                    myfile.write(markdown)
+                return HttpResponseRedirect(reverse("index"))
+
+            except FileExistsError:
+                messages.info(request, 'An entry with this name exists!')
+                return render(request, "encyclopedia/new.html", {
+                "form": form
+            })
+    
+        else:
+            return render(request, "encyclopedia/new.html", {
+                "form": form
+            })
+    else:
+        return render(request, "encyclopedia/new.html", {
+            "form": NewWikiForm()
+        })
+
+def edit(request, wiki_name):
+    if request.method == "POST":
+        form = NewWikiForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            markdown = form.cleaned_data["markdown"]
+                        
+            with open("./entries/" + title +".md", 'w') as f:
+                myfile = File(f)
+                myfile.write(markdown)
+
+            return HttpResponseRedirect(reverse("wiki", args=[wiki_name]))
+
+         
+    
+        else:
+            return render(request, "encyclopedia/new.html", {
+                "form": form
+            })
+
+    entry = util.get_entry(wiki_name)
+    form = NewWikiForm(initial={'title': wiki_name, 'markdown': entry})
+
+    return render(request, "encyclopedia/edit.html", {
+        "form": form,
+        "entry_name": wiki_name
+
+
+    })
+
+
+# entry = util.get_entry(wiki_name)
+
+#     if entry: 
+
+#         return render(request, "encyclopedia/wiki.html", {
+#             "entry": markdown2.markdown(entry)
+#         })
+#     else:
+#         return render(request, "encyclopedia/error.html")
+   
